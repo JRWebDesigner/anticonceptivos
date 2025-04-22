d12c2a3f27eb2505b60e8847e5215ff3fa967c81
JRwebdesing <jowebdesigc@gmail.com>
2025-04-22 11:21:20 -0400

    fin front

---
+++ src/app/estadisticas/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function EstadisticasPage() {
+  return (
+    <main className="min-h-screen bg-white flex flex-col items-center p-4">
+      <h1 className="text-3xl font-bold text-indigo-700 mb-4">Estadísticas del Cuestionario</h1>
+    </main>
+  );
+}
+

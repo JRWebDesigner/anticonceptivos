82e6aa61550594f6418f2c3a88b280163ccf42b4
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-05-19 09:48:11 -0400

    Update saveResponses.ts

--- src/lib/saveResponses.ts
+++ src/lib/saveResponses.ts
@@ -1,12 +1,13 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
-export const saveResponses = async (answers: (number | null)[], userName: string, userAge: number) => {
+export const saveResponses = async (answers: (number | null)[], userName: string, userAge: number, userGender:string,) => {
   try {
     await addDoc(collection(db, "responses"), {
       answers,
       userName,
       userAge,
+      userGender,
       timestamp: new Date()
     });
     console.log("Respuestas guardadas correctamente");
